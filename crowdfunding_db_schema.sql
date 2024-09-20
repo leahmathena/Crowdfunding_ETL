a17732966751b5e4ce03fd0cc8a7d9a19eaa944f
leahmathena <leahashleymann@gmail.com>
2024-09-19 21:42:39 -0500

    updated schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,12 +1,9 @@
--- Drop tables if they exist
-DROP TABLE IF EXISTS Campaign, Contacts, Subcategory, Category CASCADE;
-
 -- Create Contacts table
 CREATE TABLE Contacts (
     contact_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
 -- Create Category table
@@ -24,17 +21,23 @@ CREATE TABLE Subcategory (
 -- Create Campaign table
 CREATE TABLE Campaign (
     cf_id SERIAL PRIMARY KEY,
-    contact_id INT REFERENCES Contacts(contact_id) ON DELETE CASCADE,
+    contact_id INT NOT NULL REFERENCES Contacts(contact_id),
     company_name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     goal NUMERIC(10, 2) NOT NULL,
     pledged NUMERIC(10, 2) NOT NULL,
     outcome VARCHAR(50) NOT NULL,
     backers_count INT NOT NULL,
-    country VARCHAR(2) NOT NULL,
-    currency VARCHAR(3) NOT NULL,
-    launch_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    category_id VARCHAR(10) REFERENCES Category(category_id) ON DELETE CASCADE,
-    subcategory_id VARCHAR(10) REFERENCES Subcategory(subcategory_id) ON DELETE CASCADE
+    country VARCHAR(10) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(10) NOT NULL REFERENCES Category(category_id),
+    subcategory_id VARCHAR(10) NOT NULL REFERENCES Subcategory(subcategory_id)
 );
+
+-- Select all records from each table
+SELECT * FROM Campaign;
+SELECT * FROM Category;
+SELECT * FROM Subcategory;
+SELECT * FROM Contacts;
